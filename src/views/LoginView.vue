559168624ec1987f616003dc3bc27979c105ec6e
Mac <louis.niuniu@gmail.com>
2023-04-17 13:42:14 +0200

    add webpack and login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="login-box">
+        <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="100px"
+    class="demo-ruleForm"
+  >
+
+    <el-form-item label="Username" prop="Username">
+      <el-input v-model="ruleForm.username" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="Password" prop="Password">
+      <el-input
+        v-model="ruleForm.password"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
+    </div>
+
+</template>
+
+
+<script lang="ts" >
+import { defineComponent, reactive, toRefs } from "vue";
+import { LoginData } from "../type/login";
+export default defineComponent(
+    {
+        setup(){const data = reactive( new LoginData());
+        const rules =  {
+                username:[
+                    {
+                        required: true,
+                        message: "please input activity name",
+                        trigger: "blur",
+                    },
+                    {
+                        min: 3,
+                        max: 5,
+                        message: "length should be 3 to 5",
+                        trigger: "blue",
+                    }
+                ],
+                password:[
+                    {
+                        required: true,
+                        message: "please input activity password",
+                        trigger: "blur",
+                    },
+                    {
+                        min: 3,
+                        max: 5,
+                        message: "length should be 3 to 5",
+                        trigger: "blue",
+                    }
+                ],
+            }
+        return{...toRefs(data), rules};
+    
+    }
+        
+    }
+);
+
+
+</script>
+
+<style  lang="scss" scoped>
+    .login-box{
+        widows: 100%;
+        height: 100%;
+        background: url(../Desert1.jpg);
+
+
+    }
+    .demo-ruleForm{
+        width: 500px;
+        margin: 200px;
+        background-color: white;
+        padding: 30px;
+        border-radius: 20px;
+
+    }
+
+</style>
